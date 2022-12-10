90abd093acbaa2e7a91b4cea0088ade7a26ac0e9
NeuralPrg <chabaneyann@gmail.com>
2022-12-10 10:55:59 +0100

    Run anim + Turn
    Run animation + Turn function added to the playercontroller

--- Test_tuto_yt/Assets/Scripts/PlayerMovement.cs
+++ Test_tuto_yt/Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D rb;
+    private Animator myAnimator;
+
+    public float speed = 2.0f;
+    public float horizMovement;
+    private bool facingRight = true;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        horizMovement = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizMovement * speed, rb.velocity.y);
+        Flip(horizMovement);
+        myAnimator.SetFloat("Speed", Mathf.Abs(horizMovement));
+    }
+
+    private void Flip(float horizontal)
+    {
+        if(horizontal < 0 && facingRight ||  horizontal > 0 && !facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
     }
 }
