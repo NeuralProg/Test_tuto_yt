25330f44b303aa8f259ea685eb2034fee51a18aa
NeuralPrg <chabaneyann@gmail.com>
2022-12-26 20:12:10 +0100

    OOP script creation

---
+++ Test_tuto_yt/Assets/Characters/Character.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    [Header("Movement Variables")]
+    [Header("Jump Variables")]
+    [Header("Dash Variables")]
+    [Header("Attack Variables")]
+    [Header("Character Stats")]
+}
